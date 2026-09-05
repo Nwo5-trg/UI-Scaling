9e6958a59e162620213b62dc0c56f3bb5be7e935
Fei <cubecereal@gmail.com>
2026-09-05 13:08:30 -0400

    ill js add the cache in 2.1.0 but i wanna get this bugfix out first

--- src/layers/tinker.cpp
+++ src/layers/tinker.cpp
@@ -107,9 +107,14 @@ namespace uiscaling::tinker {
             ret.useCustomSafeArea = ret.useCustomSafeAreaSetting->getValue();
             ret.useCustomSafeAreaSetting->setValue(pCustomSafeArea.has_value());
         }
-        if ((ret.customSafeAreaSetting = std::static_pointer_cast<FloatSettingV3>(tinker->getSetting("UIScaling-custom-safe-area"))) && pCustomSafeArea.has_value()) {
-            ret.customSafeArea = ret.customSafeAreaSetting->getValue();
-            ret.customSafeAreaSetting->setValue(pCustomSafeArea.value());
+        if ((ret.customSafeAreaSetting = std::static_pointer_cast<FloatSettingV3>(tinker->getSetting("UIScaling-custom-safe-area")))) {
+            if (pCustomSafeArea.has_value()) {
+                ret.customSafeArea = ret.customSafeAreaSetting->getValue();
+                ret.customSafeAreaSetting->setValue(pCustomSafeArea.value());
+            }
+            else {
+                ret.customSafeAreaSetting = nullptr;
+            }
         }
         if ((ret.scaleSetting = std::static_pointer_cast<FloatSettingV3>(tinker->getSetting("UIScaling-scale")))) {
             ret.scale = ret.scaleSetting->getValue();
